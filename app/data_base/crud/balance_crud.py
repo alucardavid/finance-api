69ee5fd6c928ae0956699812e29d81a27843ba91
David Pereira <d_pereira@outlook.com.br>
2024-08-06 20:42:46 -0300

    create crud update, delete and put to balance api

--- app/data_base/crud/balance_crud.py
+++ app/data_base/crud/balance_crud.py
@@ -4,10 +4,16 @@ from ..schemas import balance_schema
 from ..models import balance_model
 
 def get_balances(db: Session, skip: int = 0, limit: int = 100):
+    """Get all balances"""
     return db.query(balance_model.Balance).offset(skip).limit(limit).all()
 
+def get_balance_by_id(db: Session, balance_id: int):
+    """Get a balance by Id"""
+    balance = db.query(balance_model.Balance).get(balance_id)
+    return balance
+
 def create_balance(db: Session, balance: balance_schema.BalanceCreate):
-    print(datetime.now)
+    """Create a new balance"""
     db_balance = balance_model.Balance(
         description= balance.description,
         value= balance.value,
@@ -21,3 +27,36 @@ def create_balance(db: Session, balance: balance_schema.BalanceCreate):
     db.refresh(db_balance)
 
     return db_balance
+
+def delete_balance(db: Session, balance_id):
+    """Delete a balance by id"""
+    db_balance = db.query(balance_model.Balance).get(balance_id)
+    if db_balance is not None:
+        db.delete(db_balance)
+        db.commit()
+        return balance_id
+    else:
+        return None
+    
+def update_balance(db: Session, balance_id: int, new_balance: balance_schema.BalanceUpdate):
+    """Update a balance by id"""
+    db_balance = db.query(balance_model.Balance).get(balance_id)
+    if db_balance is not None:
+        if new_balance.description is not None:
+            db_balance.description = new_balance.description
+            
+        if new_balance.value is not None:
+            db_balance.value = new_balance.value
+
+        if new_balance.show is not None:
+            db_balance.show = new_balance.show  
+        
+        db_balance.updated_at = datetime.now()
+        db.commit()
+        db.refresh(db_balance)
+        return db_balance
+    else:
+        return None
+
+    
+

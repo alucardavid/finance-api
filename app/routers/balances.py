4e98be3adda272aa6729acb30c8125143b260f61
David Pereira <d_pereira@outlook.com.br>
2024-08-10 22:19:54 -0300

    add monthly expense modulo, crud and model

--- app/routers/balances.py
+++ app/routers/balances.py
@@ -3,19 +3,11 @@ from sqlalchemy.orm import Session
 from ..data_base.models import balance_model
 from ..data_base.crud import balance_crud
 from ..data_base.schemas import balance_schema
-from ..data_base.database import SessionLocal, engine
+from ..data_base.database import SessionLocal, engine, get_db
 
 balance_model.Base.metadata.create_all(bind=engine)
 
 router = APIRouter()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 
 @router.get("/", status_code=201)
 def read_balances(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):

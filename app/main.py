d53bc24e6e90bd36f1dee4eb19b36c99e3f54bab
David Pereira <d_pereira@outlook.com.br>
2024-09-08 18:39:23 -0300

    include cors config

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
 from fastapi import Depends, FastAPI
 from .routers import balances, monthly_expenses, variable_expenses, form_of_payments
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:8001",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(balances.router, prefix="/balances", tags=["balances"])
 app.include_router(monthly_expenses.router, prefix="/monthly-expenses", tags=["monthly-expenses"])

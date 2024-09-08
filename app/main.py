bdaa2eb5bfc14383f4fd7756c2933e8cdfcf6c88
David Pereira <d_pereira@outlook.com.br>
2024-09-08 16:43:07 -0300

    fix tags doc swagger

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ app = FastAPI()
 
 app.include_router(balances.router, prefix="/balances", tags=["balances"])
 app.include_router(monthly_expenses.router, prefix="/monthly-expenses", tags=["monthly-expenses"])
-app.include_router(variable_expenses.router, prefix="/variable-expenses", tags="variable-expenses")
-app.include_router(form_of_payments.router, prefix="/form-of-payments", tags="form-of-payments")
+app.include_router(variable_expenses.router, prefix="/variable-expenses", tags=["variable-expenses"])
+app.include_router(form_of_payments.router, prefix="/form-of-payments", tags=["form-of-payments"])
 
 @app.get("/")
 async def root():

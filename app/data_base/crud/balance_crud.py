07d9d1a247fb9014c687b6fcc320a464cfa424e0
David Pereira <d_pereira@outlook.com.br>
2025-08-09 18:35:40 -0300

    feat: enhance Balance model and schema with new fields; update create_balance function to handle multiple fields

--- app/data_base/crud/balance_crud.py
+++ app/data_base/crud/balance_crud.py
@@ -1,8 +1,10 @@
+from fastapi.logger import logger
 from sqlalchemy.orm import Session
 from datetime import datetime
 from ..schemas import balance_schema
 from ..models import balance_model
 from sqlalchemy.sql import text
+
 
 def get_balances(db: Session, skip: int = 0, limit: int = 100, order_by: str = "id asc"):
     """Get all balances"""
@@ -43,18 +45,17 @@ def update_balance(db: Session, balance_id: int, new_balance: balance_schema.Bal
     """Update a balance by id"""
     db_balance = db.query(balance_model.Balance).get(balance_id)
     if db_balance is not None:
-        if new_balance.description is not None:
-            db_balance.description = new_balance.description
-            
-        if new_balance.value is not None:
-            db_balance.value = new_balance.value
+        fields = ["description", "value", "show", "status_open_finance", "id_item", "id_account_bank"]
+        for field in fields:
+            new_value = getattr(new_balance, field, None)
+            if new_value is not None:
+                setattr(db_balance, field, new_value)
 
-        if new_balance.show is not None:
-            db_balance.show = new_balance.show  
         
         db_balance.updated_at = datetime.now()
         db.commit()
         db.refresh(db_balance)
+        logger.info(f"Balance {balance_id} updated successfully. New values: {new_balance}")
         return db_balance
     else:
         return None
